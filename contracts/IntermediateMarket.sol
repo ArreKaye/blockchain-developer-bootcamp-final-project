cb6bfd07ecfb60fc70e74834de15401767084e50
arrekaye <rhk590@gmail.com>
2021-11-30 17:35:35 -0500

    Upgrade Skeleton

---
+++ contracts/IntermediateMarket.sol
@@ -0,0 +1,13 @@
+// Add Notes
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./SimpleMarket.sol";
+
+contract IntermediateEvents {
+   
+}
+
+contract IntermediateMarket is SimpleMarket, IntermediateEvents {
+
+}
